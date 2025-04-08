b470e917f6f87b7775d7c79638bea49c924da2b8
Ryan Pratt <ryan.pratt@openc3.com>
2025-04-08 14:33:21 -0600

    annual neovim config thing lol

--- .config/nvim/lua/ryan-pratt/packer.lua
+++ .config/nvim/lua/ryan-pratt/packer.lua
@@ -17,12 +17,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
   use({
 	  'rebelot/kanagawa.nvim',
 	  config = function()
@@ -30,6 +24,12 @@ return require('packer').startup(function(use)
 	  end
   })
 
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use('nvim-tree/nvim-web-devicons')
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
@@ -38,20 +38,38 @@ return require('packer').startup(function(use)
   use('mfussenegger/nvim-jdtls')
 
   use {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup {}
+    end
+  }
+
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  })
+
+  use {
 	  'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x',
+    branch = 'v3.x',
 	  requires = {
-          --- Uncomment the two plugins below if you want to manage the language servers from neovim
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
+      --- Uncomment the two plugins below if you want to manage the language servers from neovim
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
 
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},
-          {'hrsh7th/cmp-nvim-lsp'},
-          {'L3MON4D3/LuaSnip'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
 	  }
   }
 
