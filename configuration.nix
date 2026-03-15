d01e9d943e55352c12537eb39af622ea6d969bc7
Ryan Pratt <ryan.pratt@live.com>
2026-03-15 13:47:44 -0600

    add delta

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,7 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     brightnessctl
+    delta
     git
     git-lfs
     openvpn
