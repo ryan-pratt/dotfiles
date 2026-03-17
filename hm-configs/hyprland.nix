4a718d4c755b43348c4ca3e3dca91179f07ab952
Ryan Pratt <ryan.pratt@live.com>
2026-03-17 00:24:36 -0600

    smart gaps

--- hm-configs/hyprland.nix
+++ hm-configs/hyprland.nix
@@ -101,6 +101,25 @@
       windowrule = [
         "suppressevent maximize, class:.*"
       ];
+
+      workspace = [
+        # smart gaps
+        "w[t1], gapsout:0, gapsin:0"
+        "w[tg1], gapsout:0, gapsin:0"
+        "f[1], gapsout:0, gapsin:0"
+        # end smart gaps
+      ];
+
+      windowrulev2 = [
+        # smart gaps
+        "bordersize 0, floating:0, onworkspace:w[t1]"
+        "rounding 0, floating:0, onworkspace:w[t1]"
+        "bordersize 0, floating:0, onworkspace:w[tg1]"
+        "rounding 0, floating:0, onworkspace:w[tg1]"
+        "bordersize 0, floating:0, onworkspace:f[1]"
+        "rounding 0, floating:0, onworkspace:f[1]"
+        # end smart gaps
+      ];
     };
   };
 }
