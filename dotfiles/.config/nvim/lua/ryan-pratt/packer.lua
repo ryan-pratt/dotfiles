184224b0cb55b04cd0bfad4f223df8937a3e3eb1
Ryan Pratt <ryan.pratt@openc3.com>
2026-07-07 13:41:23 -0600

    fix treesitter

--- dotfiles/.config/nvim/lua/ryan-pratt/packer.lua
+++ dotfiles/.config/nvim/lua/ryan-pratt/packer.lua
@@ -31,7 +31,6 @@ return require('packer').startup(function(use)
 
   use('nvim-tree/nvim-web-devicons')
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
