8781d646ef45d25601f9030ba652579834c8088d
Ryan Pratt <ryan.pratt@openc3.com>
2026-07-07 13:56:03 -0600

    fix vue comments

--- dotfiles/.config/nvim/after/plugin/treesitter.lua
+++ dotfiles/.config/nvim/after/plugin/treesitter.lua
@@ -1,3 +1,10 @@
-require('nvim-treesitter').install({
-  'vimdoc', 'javascript', 'typescript', 'c', 'lua', 'rust', 'vue', 'ruby',
+local langs = { 'vimdoc', 'javascript', 'typescript', 'c', 'lua', 'rust', 'vue', 'ruby' }
+
+require('nvim-treesitter').install(langs)
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'vue', 'ruby', 'rust', 'javascript', 'typescript' },
+  callback = function(ev)
+    pcall(vim.treesitter.start, ev.buf)
+  end,
 })
