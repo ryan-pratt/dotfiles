533f32f232743d8e9dd3867d7932aab0e845a783
Ryan Pratt <ryan.pratt@live.com>
2026-08-12 23:54:04 -0600

    manage gitconfig with home-manager

--- home.nix
+++ home.nix
@@ -20,12 +20,47 @@
       };
     };
   in
-    builtins.listToAttrs (map (mapDotfiles) dotfilesContent);
+    builtins.listToAttrs (map (mapDotfiles) dotfilesContent) // {
+      ".config/git/allowed-signers".text = ''
+        ryan.pratt@live.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILT0TbEeh/NUprEai8/7BsxS64ayqlqPDRvdCuA+lz2l
+      '';
+    };
 
   home.packages = with pkgs; [
     pkgs.nerd-fonts.fira-code
     inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default
   ];
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+    signing = {
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
+
+    settings = {
+      user.name = "Ryan Pratt";
+      user.email = "ryan.pratt@live.com";
+      gpg.format = "ssh";
+      "gpg \"ssh\"".allowedSignersFile = "~/.config/git/allowed-signers";
+      tag.gpgsign = true;
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      features = "decorations";
+      side-by-side = true;
+      line-numbers = true;
+    };
+  };
 
   programs.keepassxc = {
     enable = true;
