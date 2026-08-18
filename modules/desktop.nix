b1d93db8ecce35dc472d7a80a0d9143b48379bf7
Ryan Pratt <ryan.pratt@live.com>
2026-08-17 21:28:13 -0600

    add game streaming host config

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -16,19 +16,6 @@
     implementation = "broker";
   };
 
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-  services.printing = {
-    enable = true;
-    drivers = with pkgs; [
-      cups-filters
-      cups-browsed
-    ];
-  };
-
   programs.firefox.enable = true;
 
   services.gnome.gcr-ssh-agent.enable = false;
