e46ac780afd7df37c2d7528e0f04f685bcbf950a
Ryan Pratt <ryan.pratt@live.com>
2026-08-17 23:33:29 -0600

    make lock screen prettier

--- home/desktop.nix
+++ home/desktop.nix
@@ -5,13 +5,13 @@ let
     wallpapers=$(${pkgs.awww}/bin/awww query 2>/dev/null)
     images=$(echo "$wallpapers" | grep -oP 'image: \K.*')
     if [[ -z "$images" ]]; then
-      ${pkgs.swaylock}/bin/swaylock -fF
+      ${pkgs.swaylock-effects}/bin/swaylock -fF
     else
       args=()
       while IFS= read -r image; do
         args+=("--image" "$image")
       done <<< "$images"
-      ${pkgs.swaylock}/bin/swaylock -f "''${args[@]}"
+      ${pkgs.swaylock-effects}/bin/swaylock -f "''${args[@]}"
     fi
   '';
 
@@ -57,12 +57,43 @@ in
 
   programs.swaylock = {
     enable = true;
+    package = pkgs.swaylock-effects;
     settings = {
       show-failed-attempts = true;
       scaling = "fill";
+      effect-blur = "7x5";
+      clock = true;
+      timestr = "%I:%M %p";
+      datestr = "%A, %B %e";
+      color = "000000cc";
+      indicator = true;
+      indicator-radius = 100;
+      indicator-thickness = 10;
+      ring-color = "ffffff22";
+      ring-clear-color = "88c0d0aa";
+      ring-ver-color = "81a1c1aa";
+      ring-wrong-color = "bf616aaa";
+      key-hl-color = "a3be8caa";
+      bs-hl-color = "bf616aaa";
+      text-color = "ffffffee";
+      text-clear-color = "ffffffee";
+      text-ver-color = "ffffffee";
+      text-wrong-color = "ffffffee";
+      inside-color = "00000088";
+      inside-clear-color = "88c0d033";
+      inside-ver-color = "81a1c133";
+      inside-wrong-color = "bf616a33";
+      separator-color = "00000000";
+      line-color = "00000000";
+      line-clear-color = "00000000";
+      line-ver-color = "00000000";
+      line-wrong-color = "00000000";
+      layout-text-color = "ffffffaa";
     };
   };
+
   services.swaync.enable = true;
+
   services.swayidle = {
     enable = true;
     timeouts = [
